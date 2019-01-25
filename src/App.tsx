002b9a61ab61f1af272c6276754fbe3d28299f10
Daniel Yang <shu.yang@skipthedishes.ca>
2019-01-25 02:07:06 -0600

    Added svg and useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import './App.css';
-import LightbulbSvg from './LightBulb';
+import React, { useState } from 'react'
+import './App.css'
+import LightbulbSvg from './LightBulb'
 
 const App: React.FunctionComponent = () => {
+  const [light, setLight] = useState<number>(0)
+
+  const setOff = (): void => setLight(0)
+
+  const setOn = (): void => setLight(1)
+
+  const fillColor: string = light === 1 ? '#ffbb73' : '#000'
+
   return (
     <div className='App'>
       <div>
-        <LightbulbSvg fillColor='blue' />
+        <LightbulbSvg fillColor={fillColor} />
       </div>
+
+      <button onClick={setOff}>Off</button>
+      <button onClick={setOn}>On</button>
     </div>
   )
 }
 
-export default App;
+export default App
